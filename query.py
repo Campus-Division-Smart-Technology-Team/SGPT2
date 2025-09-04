bef273aa1f3a26fe859af5284af7532e9d5ad94f
Gabriel Olabode <111546959+koye007@users.noreply.github.com>
2025-09-04 10:15:43 +0100

    Update query.py

--- query.py
+++ query.py
@@ -364,7 +364,7 @@ with st.sidebar:
         namespace = "" if selected_label == "(default)" else selected_label
     else:
         # Fallback in case there are no indexes yet
-        namespace = st.text_input("Namespace", value="default")
+        namespace = st.text_input("Namespace", value="__default__")
 
     top_k = st.slider("Top K", min_value=1, max_value=25, value=5)
 
